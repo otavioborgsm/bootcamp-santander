d27ab76081b39711c65aae581992debf649e4618
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-04 11:16:30 -0300

    versão final do projeto

--- bankline-api/src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ bankline-api/src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -2,12 +2,21 @@ package com.dio.santander.bankline.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.dio.santander.bankline.api.model.Correntista;
 
 @SpringBootApplication
 public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
+		
+	}
+	
+	@PostMapping
+	public void save(@RequestBody Correntista correntista) {
 		
 	}
 
